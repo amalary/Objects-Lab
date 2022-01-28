8dfc3099c307ef822d6471f96897c450e8143043
Anthony Malary <amalary@bu.edu>
2022-01-28 13:57:40 -0800

    Finished question 1 and two on Objects Lab

---
+++ ObjectLab.js
@@ -0,0 +1,35 @@
+// Syntax 
+
+
+let videoGames = {
+    Nintento:"Pokemon",
+    Playstation:"GTA San Andreas",
+}; 
+
+// there is a key in this case being nintendo and playstation and the values are the strings after them. Curly braces make objects. 
+
+let me = {
+    name: "Anthony", 
+    age: 26,
+    email:'amalarwey@zooellow.com', 
+}; 
+
+console.log(me); 
+
+console.log(me.name); 
+
+me.age = 1000; 
+
+console.log(me.age); 
+
+let me = {
+    name: "Anthony", 
+    age: 26,
+    email:'amalarwey@zooellow.com', 
+}; 
+
+me.placeOfResidence = "Los Angeles"; 
+
+console.log(me.placeOfResidence); 
+
+
